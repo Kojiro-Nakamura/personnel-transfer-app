f1e4efcfdb8a24ba7df29f36e2915d294ef33b23
Kojiro-Nakamura <Kojiro-Nakamura@users.noreply.github.com>
2026-07-26 07:08:28 +0900

    Clean up script files

--- fixDivs.cjs
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-const file = 'src/components/employee/EmployeeComponents.jsx';
-let lines = fs.readFileSync(file, 'utf8').split('\n');
-
-lines.splice(430, 2);
-
-fs.writeFileSync(file, lines.join('\n'), 'utf8');
